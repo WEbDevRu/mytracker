c4f3e080a47d99ddd259c2611409cbc5274f3edf
KRAINEV <nikrainev@gmail.com>
2021-02-09 00:54:34 +0300

    Фиксы и изменения названий в сторону REST

--- api/routes/profileInfo/profileActions.js
+++ api/routes/profileInfo/profileActions.js
@@ -37,42 +37,58 @@ router.delete("/proposal/:userId", checkAuth, (req,res)=>{
 })
 
 //Работа с заявками (приём или отклонение)
-router.put("/proposals/", checkAuth, (req,res)=>{
+
+
+
+
+
+router.post("/proposals/:friendId", checkAuth, (req,res)=>{
     ProfileInfo.findOne({_id: req.userData.userId}).then(docs =>{
         // Проверяем что такая завявка есть
-        if(docs.proposals.indexOf(req.body.friendId) != -1){
-
-            if(req.body.action === 'accept'){
+        if(docs.proposals.indexOf(req.params.friendId) != -1){
                 ProfileInfo
-                    .findOneAndUpdate({_id: req.body.friendId}, {$addToSet: {friends: req.userData.userId},
-                        $addToSet: {newFriends: {accepted: req.userData.userId}}}).then(doc=>{
+                    .findOneAndUpdate({_id: req.params.friendId}, {$addToSet: {friends: req.userData.userId},
+                        $push: {newFriends: {accepted: req.userData.userId}}}).then(doc=>{
                     ProfileInfo
-                        .findOneAndUpdate({_id: req.userData.userId}, {$addToSet: {friends: req.body.friendId}, $pull: {proposals: req.body.friendId}})
+                        .findOneAndUpdate({_id: req.userData.userId}, {$addToSet: {friends: req.params.friendId}, $pull: {proposals: req.params.friendId}})
                         .then(doc=>{
                             res.status(200).json({message: "Friend added"})
                         })
                 }).catch(error=>{
                     res.status(500).json({message: "Something go wrong"})
                 })
-            }
-            else if (req.body.action === 'deny'){
-
-                ProfileInfo
-                    .findOneAndUpdate({_id: req.userData.userId}, {$pull: {proposals: req.body.friendId}}).then(doc=>{
-                    ProfileInfo
-                        .findOneAndUpdate({_id: req.body.friendId}, {$addToSet: {newFriends: {denied: req.userData.userId}}}).then(doc=>{
-                        res.status(200).json({message: "Proposal denied"})
-                    })
-                }).catch(error=>{
-                    res.status(500).json({message: "Something go wrong"})
-                })
-            }
-
         }
         else{
             res.status(404).json({message: "You have not this proposal"})
         }
     }
+    )
+})
+
+
+
+router.delete("/proposals/:friendId", checkAuth, (req,res)=>{
+    ProfileInfo.findOne({_id: req.userData.userId}).then(docs =>{
+            // Проверяем что такая завявка есть
+            if(docs.proposals.indexOf(req.params.friendId) != -1){
+
+
+                    ProfileInfo
+                        .findOneAndUpdate({_id: req.userData.userId}, {$pull: {proposals: req.params.friendId}}).then(doc=>{
+                        ProfileInfo
+                            .findOneAndUpdate({_id: req.params.friendId}, {$addToSet: {newFriends: {denied: req.userData.userId}}}).then(doc=>{
+                            res.status(200).json({message: "Proposal denied"})
+                        })
+                    }).catch(error=>{
+                        res.status(500).json({message: "Something go wrong"})
+                    })
+
+
+            }
+            else{
+                res.status(404).json({message: "You have not this proposal"})
+            }
+        }
     )
 
 
@@ -101,8 +117,14 @@ router.delete("/delete_friend/:friendId", checkAuth, (req,res)=>{
             ProfileInfo
                 .findOneAndUpdate({_id:req.userData.userId}, {$pull: {friends: req.params.friendId}})
                 .then(doc=>{
+                    ProfileInfo
+                        .findOneAndUpdate({_id:req.params.friendId}, {$pull: {friends: req.userData.userId}}).then(doc=>{
+
                     res.status(200).json({message: "friend deleted"})
-                })
+                    })
+                }).catch(error=>{
+                res.status(500).json({message: "Something go wrong"})
+            })
 
         }
         else{
