7c42d49b8243cbc7efd2309166d030dd5c2d0c8f
KRAINEV <nikrainev@gmail.com>
2021-03-26 00:02:03 +0300

    Эндпоинт вывода информации для счётчика

--- api/controllers/counters/countersInfo.js
+++ api/controllers/counters/countersInfo.js
@@ -13,11 +13,13 @@ let getCounterWithUsers = (counters) =>{
     dayBeginTime =  Date.now() - dayBeginTime
     let countersWithUsers = [...counters]
 
+
     return User
         .find({counterId: countersId, lastSession: {$gte: dayBeginTime}})
         .then(users =>{
 
             countersWithUsers.forEach((counter, counterIndex) =>{
+
                 users.forEach((user)=>{
                     if(counter._id.toString() === user.counterId.toString()){
                         console.log(countersWithUsers[counterIndex].dayusers)
@@ -47,11 +49,11 @@ exports.get_profile_counters = (async (req, res, next) => {
            return Auth
                 .findOne({_id: req.userData.userId})
                 .then(user=>{
-                    return counterWithLogin.map((item) =>({
+                    counterWithLogin = counterWithLogin.map((item) =>({
                         login: user.login,
                         ...item._doc
                     }))
-
+                    return counterWithLogin
                 })
         })
         .then(counters =>{
@@ -114,7 +116,7 @@ exports.get_friends_counters = (async (req, res, next) => {
             return Promise.all(arrayPromises).then(counters => (counters))
         })
         .then(counters =>{
-
+            console.log(counters)
             return getCounterWithUsers(counters)
 
         })
@@ -135,41 +137,49 @@ exports.get_friends_counters = (async (req, res, next) => {
 
 
 
-exports.get_counter_byId = (req, res) => {
-
-    Counter
-        .findOne({profileId: req.userData.userId, _id: req.params.counterId})
-        .exec()
-        .then(doc =>{
-            if(doc){
-
-                getCounterWithUsers([doc]).then(counter=>{
-                   console.log(counter)
-                    res.status(200).json({
-                        _id: counter._id,
-                        name: counter.name,
-                        domen: counter.domen,
-                        dayusers: counter.dayusers,
-                        allusers: counter.allusers,
-                        pixelCode: "<script>\n" +
-                            "\tlet script = document.createElement('script');\n" +
-                            "\tscript.src = \"https://trackyour.site/scripts/pixel.js\"\n" +
-                            "\tdocument.head.append(script);\n" +
-                            "\tscript.onload = () => {\n" +
-                            "  \t\ttrackerInit("+"'"+doc._id+"'"+")\n" +
-                            "\t}\t\n" +
-                            "</script>"
-                    })
-                })
-
-            }
-            else{
-                res.status(404).json({message: "you do not have such a counter"})
-            }
-
-        })
+exports.get_counter_byId = (async (req, res) => {
+    let counter = await Profile
+        .findOne({_id: req.userData.userId})
+        .then(profile=>{
+            return Counter
+                .findOne({profileId: [req.userData.userId, ...profile.friends], _id: req.params.counterId})
+                .then(counter=>{
+                    return counter
+            })
+            })
         .catch(err =>{
-            res.status(500).json(err)
+            res.status(500).json({error: err})
         })
 
-}
+    let login = await Auth
+        .findOne({_id: counter.profileId})
+        .then(user=>{
+            return user.login
+        })
+
+
+    if(counter){
+        let date = new Date()
+        let dayBeginTime = date.getHours()*60*60*1000 + date.getMinutes()*60*1000 +
+            date.getSeconds()*1000 + date.getMilliseconds()
+        dayBeginTime =  Date.now() - dayBeginTime
+
+        User
+            .find({counterId: req.params.counterId, lastSession: {$gte: dayBeginTime}})
+            .then(users=>{
+                console.log(counter)
+                let newCounter = {...counter._doc}
+                newCounter.dayusers = users.length
+                newCounter.login = login
+                res.status(200).json(newCounter)
+            })
+            .catch(err =>{
+            res.status(500).json({error: err})
+            })
+    }
+    else{
+        res.status(404).json({message: "you do not have such a counter"})
+    }
+
+
+})
